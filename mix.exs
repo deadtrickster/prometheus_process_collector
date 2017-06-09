4740b867a318bcb1a9c84cf5c2bae7e7e45590c3
Ilya Khaprov <dead.trickster@gmail.com>
2017-06-09 17:15:27 +0300

    Bump to v1.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrometheusProcessCollector.Mixfile do
 
   def project do
     [app: :prometheus_process_collector,
-     version: "1.0.3",
+     version: "1.1.0",
      description: description(),
      package: package(),
      deps: deps()]
