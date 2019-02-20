4fb3d39dc73f28000c1f6f866ade65ad68cbf528
Ilya Khaprov <i.khaprov@gmail.com>
2019-02-20 08:11:55 +0100

    Bump to v1.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PrometheusProcessCollector.Mixfile do
   def project do
     [
       app: :prometheus_process_collector,
-      version: "1.4.0",
+      version: "1.4.1",
       compilers: [:elixir_make] ++ Mix.compilers(),
       # make_cwd: "c_src",
       description: description(),
